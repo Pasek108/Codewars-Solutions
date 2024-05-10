6720b31ef835f7afe6189314bfea041a9cb7683e
Artur Pas <pasartur108@gmail.com>
2024-05-10 23:18:22 +0200

    challenges data

---
+++ Scripts/8kyu_data.js
@@ -0,0 +1,10 @@
+"use strict"
+
+const challenges_8kyu_data = [
+  {
+    solution_langauges: ["javascript", "prolog", "haskell", "nasm"],
+    link: "https://www.codewars.com/kata/50654ddff44f800200000004",
+    name: "Multiply",
+    kyu: 8,
+  },
+]
--- Scripts/script.js
+++ Scripts/script.js
@@ -1,9 +1,9 @@
 "use strict"
 
+/* ------------------------- open challeneges ------------------------- */
 const solution_content = document.querySelector(".solution-content")
 const challenges_select = document.querySelector(".challenges-select")
 
-/* ------------------------- open challeneges ------------------------- */
 const show_challenges_button = document.querySelector(".show-challenges")
 show_challenges_button.addEventListener("click", () => {
   if (solution_content.hasAttribute("hidden")) {
@@ -12,6 +12,32 @@ show_challenges_button.addEventListener("click", () => {
   } else {
     solution_content.setAttribute("hidden", true)
     challenges_select.removeAttribute("hidden")
+  }
+})
+
+/* ------------------------- info tabs ------------------------- */
+const instructions_tab = document.querySelector(".instructions-tab")
+const description_tab = document.querySelector(".description-tab")
+
+instructions_tab.addEventListener("click", () => {
+  description_tab.classList.remove("active")
+  instructions_tab.classList.add("active")
+})
+
+description_tab.addEventListener("click", () => {
+  instructions_tab.classList.remove("active")
+  description_tab.classList.add("active")
+})
+
+/* ------------------------- solution langauge ------------------------- */
+const current_language = document.querySelector(".current-language")
+const languages_list = document.querySelector(".languages-list")
+
+current_language.addEventListener("click", () => {
+  if (languages_list.hasAttribute("hidden")) {
+    languages_list.removeAttribute("hidden")
+  } else {
+    languages_list.setAttribute("hidden", true)
   }
 })
 
